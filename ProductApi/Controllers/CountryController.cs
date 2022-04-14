cfa73b086df07f698ddad59a0513638d9ed7f2c4
BigCharckle <103252842+BigCharckle@users.noreply.github.com>
2022-04-14 23:58:09 +1000

    Add files via upload

---
+++ ProductApi/Controllers/CountryController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Logic.interfaces;
+using Models.culture;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace ProductApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountryController : Controller
+    {
+        private IGenericRepositoryrReadable <Country> _countryRepository;
+
+        public CountryController(IGenericRepositoryrReadable<Country> countryRepository)
+        {
+            this._countryRepository = countryRepository;
+
+        }
+
+        [Route("{id:int}")]
+        [HttpGet]
+        public Country Details(int id)
+        {
+            return GetAll().Where(c=>c.id==id).FirstOrDefault();
+        }
+
+        [HttpGet]
+        public List<Country> GetAll()
+        {
+
+            return _countryRepository.GetAll().ToList();
+        }
+
+    }
+}
---
+++ ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Logic.interfaces;
+using Models.products;
+using Microsoft.Extensions.Configuration;
+namespace ProductApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private IRepositoryFromExternal<Product> _productRepository;
+        private readonly IConfiguration _config;
+        //private readonly ILogger<ProductController> _logger;
+        public ProductController(IRepositoryFromExternal<Product> _productRepository, IConfiguration config)
+        {
+            this._productRepository = _productRepository;
+            this._config = config;
+        }
+
+        [Route("{id:int}")]
+        [HttpGet]
+        public Product Details(int id)
+        {
+            return new Product();
+        }
+
+        // GET: ProductController/Create
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<List<Product>> GetFromExternalAsyn()
+        {
+            var url = _config.GetValue<string>("ExternalUrl");
+            return await _productRepository.GetFromExternalAsyn(url);
+        }
+
+    }
+}

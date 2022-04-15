2afb6a23077f63e9fde042d74efe21a12b90aa83
BigCharckle <103252842+BigCharckle@users.noreply.github.com>
2022-04-15 18:57:24 +1000

    Add files via upload

---
+++ BusinessObjects/transaction/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.transaction
+{
+    public class CartItem
+    {
+        public int id { get; set; }
+        public int quantity { get; set; }
+        public decimal price { get; set; }
+        public decimal itemsprice
+        {
+             get
+                    { return this.price * this.quantity; } 
+            
+        }
+    }
+}
---
+++ BusinessObjects/transaction/ShoppingCart.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Models.culture; 
+namespace Models.transaction
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart(List<CartItem> items, Country country)
+        {
+            this.cartItems = items;
+            this.country = country;
+        }
+
+        public int id { get; set; }
+        
+        public decimal itemsprice{
+            get{
+                return this.cartItems.Sum(x => x.itemsprice);
+                } 
+        }
+        public decimal shippingprice
+        {
+            get
+            {
+                return (itemsprice > 50 ? 20 : 10);
+            }
+        }
+        public decimal totalprice
+        {
+            get
+            {
+                return itemsprice + shippingprice;
+
+            }
+        }
+        public Country country { get; set; }
+
+        public List<CartItem> cartItems { get; set; }
+
+    }
+}

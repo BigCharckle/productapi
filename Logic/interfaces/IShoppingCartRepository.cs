51119fc988043d24603bf4ffc8eec10bd31aba19
BigCharckle <103252842+BigCharckle@users.noreply.github.com>
2022-04-15 19:00:15 +1000

    Add files via upload

---
+++ Logic/interfaces/IShoppingCartRepository.cs
@@ -0,0 +1,11 @@
+﻿using Models.transaction;
+
+namespace Logic.interfaces
+{
+    public interface IShoppingCartRepository
+    {
+        decimal CalculateShippingPrice(ShoppingCart shoppingCart);
+        void CheckOut(ShoppingCart shoppingCart);
+
+    }
+}
